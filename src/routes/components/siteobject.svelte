bd4c1ce3c470315a4da352e905eddc78de7cc8a2
SreejithKSGupta <sreejithksgupta2255@gmail.com>
2023-12-13 18:12:51 +0530

    default site icon

--- src/routes/components/siteobject.svelte
+++ src/routes/components/siteobject.svelte
@@ -1,80 +1,84 @@
 <script lang="ts">
-	import { scale } from 'svelte/transition'
+	import { scale } from 'svelte/transition';
 
-    import editicon from '$lib/res/delsite.svg';
-    import { settingsoptions, tooltipviews, sitelists} from '../../dbase'
-    let showSiteName = false;
-    export let site : {id:number, name: string, url: string};
+	import editicon from '$lib/res/delsite.svg';
+	import defsiteicon from '$lib/res/defsite.svg'
+	import { settingsoptions, tooltipviews, sitelists } from '../../dbase';
+	let showSiteName = false;
+	export let site: { id: number; name: string; url: string };
 	export let index: number;
 
-    function gotowebsite(url: string, target: EventTarget) {
-        if ($settingsoptions[0].value == 0) {
-            // full screen animation
-        }
-        $settingsoptions[0].value ? window.open(url, '_blank') : (window.location.href = url);
-    }
+	function gotowebsite(url: string, target: EventTarget) {
+		if ($settingsoptions[0].value == 0) {
+			// full screen animation
+		}
+		$settingsoptions[0].value ? window.open(url, '_blank') : (window.location.href = url);
+	}
 
-    function geticon(url: string) {
-        const [, , domain] = url.split('/');
-        let imgurl = `https://s2.googleusercontent.com/s2/favicons?domain=${domain}&sz=128`;
-        return imgurl;
-    }
+	function geticon(url: string) {
+		const [, , domain] = url.split('/');
+		let imgurl = `https://s2.googleusercontent.com/s2/favicons?domain=${domain}&sz=128`;
+		return imgurl;
+	}
 
-    function setdeficon(e: Event) {
-        if (e.target instanceof HTMLImageElement) {
-            e.target.src = editicon;
-        }
-    }
+	function setdeficon(e: Event) {
+		if (e.target instanceof HTMLImageElement) {
+			e.target.src = defsiteicon;
+		}
+	}
 	function deletesite(option: number) {
+        event!.stopPropagation();
 		const newTracklist = [...$sitelists];
 		newTracklist.splice(option, 1);
 		sitelists.set(newTracklist);
 	}
-	
-	
-		$: {
-    const option = $settingsoptions.find(
-        (option: { name: string }) => option.name === 'show_sitename'
-    );
-    if (option) {
-        showSiteName = option.value;
-    }
-}
+
+	$: {
+		const option = $settingsoptions.find(
+			(option: { name: string }) => option.name === 'show_sitename'
+		);
+		if (option) {
+			showSiteName = option.value;
+		}
+	}
+
 </script>
 
-<div 
-    title={site.url}
-    class="sitebtn col"
-    role="button"
-    tabindex="-1"
-    on:click={(event) => {
-        if (event.target) {
-            gotowebsite(site.url, event.target);
-        }
-    }}
-    on:keydown={(event) => {
-        if (event.key === 'Enter' && event.target) {
-            gotowebsite(site.url, event.target);
-        }
-    }}
+<div
+	title={site.url}
+	class="sitebtn col"
+	role="button"
+	tabindex="-1"
+	on:click={(event) => {
+		if (event.target) {
+			gotowebsite(site.url, event.target);
+		}
+	}}
+	on:keydown={(event) => {
+		if (event.key === 'Enter' && event.target) {
+			gotowebsite(site.url, event.target);
+		}
+	}}
 >
-    <img class="siteicon" src={geticon(site.url)} on:error={setdeficon} alt={site.url} />
+	<img class="siteicon" src={geticon(site.url)} on:error={setdeficon} alt={site.url} />
+	{#if $tooltipviews.editview}
+		<button class="delbtn" on:click={() => deletesite(index)} title="remove {site.name}">
+			<img transition:scale src={editicon} alt="edit" />
+		</button>
+	{/if}
 </div>
-{#if $tooltipviews.editview} 
-   <button class="delbtn" on:click={() => deletesite(index)} title="remove {site.name}">
-		<img transition:scale src={editicon} alt="edit" />
-	</button>
-{/if}
+
 {#if showSiteName}
-	<span>{site.name}</span>
+	<span transition:scale>{site.name}</span>
 {/if}
 
-<style >
-    	.sitebtn {
+<style>
+	.sitebtn {
 		font-size: 170%;
 		border-radius: var(--sitebr);
 		margin: calc(var(--margin) * 3);
 		background-color: white;
+		position: relative;
 	}
 	.sitebtn:hover,
 	.sitebtn:focus {
@@ -86,6 +90,13 @@
 	}
 	.delbtn,
 	.delbtn img {
-		width: calc(var(--sitewidth) * 0.3);
+		width: calc(var(--sitewidth) * 0.5);
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+        border:  30px rgba(0, 0, 0, 0.664);
+        border-radius: var(--sitebr);
 	}
+
 </style>
